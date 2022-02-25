0d9a23e86fb52e3c79726f0a8548fd3454bbb963
elihimas <arthurelihimas@gmail.com>
2022-02-25 09:41:48 -0300

    - improved the UI - created add button separating add and consolidate - added help button - added help dialog - changed README.md - added screenshots

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # android-accountant
 An Android app to count items
 
+### Screenshots: ###
+
+![phone_empty](https://github.com/elihimas/android-accountant/blob/master/screenshots/phone_empty.png)
+![phone_filled](https://github.com/elihimas/android-accountant/blob/master/screenshots/phone_filled.png)
+![phone_help](https://github.com/elihimas/android-accountant/blob/master/screenshots/phone_help.png)
+![tablet](https://github.com/elihimas/android-accountant/blob/master/screenshots/tablet.png)
+
 
 ### Technologies used: ###
 - [MVVM](https://developer.android.com/topic/libraries/architecture/viewmodel)
--- app/src/main/java/com/elihimas/accountant/ui/activities/MainActivity.kt
+++ app/src/main/java/com/elihimas/accountant/ui/activities/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.elihimas.accountant.ui.activities
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.core.view.children
+import com.elihimas.accountant.R
 import com.elihimas.accountant.viewmodels.CountViewModel
 import com.elihimas.accountant.databinding.ActivityMainBinding
 import com.elihimas.accountant.model.CountEntry
@@ -33,7 +38,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateCount(count: Int) {
-        binding.etQuantity.setText(count.toString())
+        binding.tvQuantity.text = count.toString()
     }
 
     private fun updateCountEntries(entries: List<CountEntry>) {
@@ -50,15 +55,48 @@ class MainActivity : AppCompatActivity() {
             incrementButtons.forEach { it.setOnClickListener(::onIncrementClicked) }
 
             btConsolidate.setOnClickListener { onConsolidateClicked() }
+            btAdd.setOnClickListener { onAddClicked() }
 
             rvItems.adapter = adapter
         }
     }
 
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.help_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (item.itemId == R.id.menu_item_help) {
+            showHelpDialog()
+            true
+        } else {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun showHelpDialog() {
+        AlertDialog.Builder(this)
+            .setTitle(R.string.app_name)
+            .setMessage(R.string.help_text)
+            .setPositiveButton(R.string.ok) { dialog, id ->
+                dialog.cancel()
+            }
+            .show()
+    }
+
     private fun onConsolidateClicked() {
-        val quantity = binding.etQuantity.text.toString().toIntOrNull()
+        val quantity = binding.tvQuantity.text.toString().toIntOrNull()
         quantity?.let { quantity ->
             viewModel.consolidateQuantity(quantity)
+        }
+    }
+
+    private fun onAddClicked() {
+        val quantity = binding.etQuantity.text.toString().toIntOrNull()
+        quantity?.let { quantity ->
+            viewModel.addEntry(quantity)
+            binding.etQuantity.setText("");
         }
     }
 
